fca6905b875667a131d3328bf5029d65768d3d90
denisshashkov <shashkov-56@mail.ru>
2020-08-01 15:40:03 +0300

    add move blocks

--- src/game.js
+++ src/game.js
@@ -35,4 +35,69 @@ export default class Game {
       [0, 0, 0]
     ]
   };
+
+  //Метод движения фигуры влево
+  movePieceLeft() {
+    this.activePiece.x -= 1;
+
+    if (this.isPieceOutOfBounds()) {
+      this.activePiece.x += 1;
+    }
+  }
+  //Метод движения фигуры вправо
+  movePieceRight() {
+    this.activePiece.x += 1;
+
+    if (this.isPieceOutOfBounds()) {
+      this.activePiece.x -= 1;
+    }
+  }
+  //Метод движения фигуры вниз
+  movePieceDown() {
+    this.activePiece.y += 1;
+
+    if (this.isPieceOutOfBounds()) {
+      this.activePiece.y -= 1;
+      this.lockPiece();
+    }
+  }
+  //Если часть фигуры окажется за пределами поля или наличие фигуры снизу
+  hasCollision() {
+    const {
+      y: pieceY,
+      x: pieceX,
+      blocks
+    } = this.activePiece;
+
+    for (let y = 0; y < blocks.length; y++) {
+      for (let x = 0; x < blocks[y].length; x++) {
+        if (blocks[y][x] &&
+          //Проверка находится ли блок в пределах игрового поля
+          ((this.playfield[pieceY + y] === undefined || this.playfield[pieceY + y][pieceX + x] === undefined) ||
+            //Проверка есть ли с низу другой блок
+            this.playfield[pieceY + y][pieceX + x])) {
+          return true;
+        }
+
+      }
+    }
+    return false;
+  }
+  //Перенесем значения из activePiece на игровое поле
+  //используем этод метод если достигли края поля или другой фигуры
+  lockPiece() {
+    const {
+      y: pieceY,
+      x: pieceX,
+      blocks
+    } = this.activePiece;
+
+    for (let y = 0; y < blocks.length; y++) {
+      for (let x = 0; x < blocks[y].length; x++) {
+        if (blocks[y][x]) {
+          this.playfield[pieceY + y][pieceX + x] = blocks[y][x];
+        }
+      }
+    }
+  }
 }

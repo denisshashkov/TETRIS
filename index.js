fdcf7b22719f92f24a513a038e507dbf68ca6b5e
denisshashkov <shashkov-56@mail.ru>
2020-08-07 10:24:16 +0300

    add blocks style and fixed bug

--- index.js
+++ index.js
@@ -1,7 +1,37 @@
 import Game from './src/game.js';
+import View from './src/view.js';
+
+const root = document.querySelector('#root');
 
 const game = new Game();
 
-window.game = game; //Чтобы получить доступ к объекту game
+//width = 320px
+//height = 640px
+//row = 20
+// columns = 10
+const view = new View(root, 320, 640, 20, 10);
 
-console.log(game);
+
+window.game = game;
+window.view = view;
+
+document.addEventListener('keydown', event => {
+  switch (event.keyCode) {
+    case 37: // Left Arrow
+      game.movePieceLeft();
+      view.render(game.getState());
+      break;
+    case 38: // Up Arrow
+      game.rotatePiece();
+      view.render(game.getState());
+      break;
+    case 39: // Right Arrow
+      game.movePieceRight();
+      view.render(game.getState());
+      break;
+    case 40: // Down Arrow
+      game.movePieceDown();
+      view.render(game.getState());
+      break;
+  }
+});
--- src/game.js
+++ src/game.js
@@ -2,45 +2,70 @@ export default class Game {
   score = 0;
   lines = 0;
   level = 0;
-  //Создадим игровое поле
-  playfield = [
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  ];
-  //Зададим координаты фигуры
+
+  playfield = this.createPlayfield();
+
   activePiece = {
     x: 0,
     y: 0,
     blocks: [
       [0, 1, 0],
       [1, 1, 1],
-      [0, 0, 0]
+      [0, 0, 0],
     ]
   };
+
+
+  //Создадим метод который возвращает состояние игрового поля
+  getState() {
+    //Получим копию игрового поля
+    const playfield = this.createPlayfield();
+    const {
+      y: pieceY,
+      x: pieceX,
+      blocks
+    } = this.activePiece;
+
+    for (let y = 0; y < this.playfield.length; y++) {
+      playfield[y] = [];
+
+      for (let x = 0; x < this.playfield[y].length; x++) {
+        playfield[y][x] = this.playfield[y][x];
+      }
+    }
+
+    for (let y = 0; y < blocks.length; y++) {
+      for (let x = 0; x < blocks[y].length; x++) {
+        if (blocks[y][x]) {
+          playfield[pieceY + y][
+            pieceX + x
+          ] = blocks[y][x];
+        }
+      }
+    }
+    return {
+      playfield,
+    };
+  }
+
+  //Метод создания поля
+  createPlayfield() {
+    const playfield = [];
+    for (let y = 0; y < 20; y++) {
+      playfield[y] = [];
+
+      for (let x = 0; x < 10; x++) {
+        playfield[y][x] = 0;
+      }
+    }
+    return playfield;
+  }
 
   //Метод движения фигуры влево
   movePieceLeft() {
     this.activePiece.x -= 1;
 
-    if (this.isPieceOutOfBounds()) {
+    if (this.hasCollision()) {
       this.activePiece.x += 1;
     }
   }
@@ -48,7 +73,7 @@ export default class Game {
   movePieceRight() {
     this.activePiece.x += 1;
 
-    if (this.isPieceOutOfBounds()) {
+    if (this.hasCollision()) {
       this.activePiece.x -= 1;
     }
   }
@@ -56,12 +81,35 @@ export default class Game {
   movePieceDown() {
     this.activePiece.y += 1;
 
-    if (this.isPieceOutOfBounds()) {
+    if (this.hasCollision()) {
       this.activePiece.y -= 1;
       this.lockPiece();
     }
   }
-  //Если часть фигуры окажется за пределами поля или наличие фигуры снизу
+
+  //Метод поворота фигуры
+  rotatePiece() {
+    const blocks = this.activePiece.blocks;
+    const length = blocks.length;
+    //Получим пустой массив состоящий из нулей
+    const temp = [];
+    for (let i = 0; i < length; i++) {
+      temp[i] = new Array(length).fill(0);
+    }
+    //В пустой массив вставим значения ротации фигуры
+    for (let y = 0; y < length; y++) {
+      for (let x = 0; x < length; x++) {
+        temp[x][y] = blocks[length - 1 - y][x];
+      }
+    }
+    //Заменим значение blocks у объекта activePiece
+    this.activePiece.blocks = temp;
+    //При столкновении фигура не поворачивается
+    if (this.hasCollision()) {
+      this.activePiece.blocks = blocks;
+    }
+  }
+  //Метод если часть фигуры окажется за пределами поля или наличие фигуры снизу
   hasCollision() {
     const {
       y: pieceY,
@@ -71,18 +119,21 @@ export default class Game {
 
     for (let y = 0; y < blocks.length; y++) {
       for (let x = 0; x < blocks[y].length; x++) {
-        if (blocks[y][x] &&
+        if (
+          blocks[y][x] &&
           //Проверка находится ли блок в пределах игрового поля
-          ((this.playfield[pieceY + y] === undefined || this.playfield[pieceY + y][pieceX + x] === undefined) ||
+          (this.playfield[pieceY + y] === undefined ||
+            this.playfield[pieceY + y][pieceX + x] === undefined ||
             //Проверка есть ли с низу другой блок
-            this.playfield[pieceY + y][pieceX + x])) {
+            this.playfield[pieceY + y][pieceX + x])
+        ) {
           return true;
         }
-
       }
     }
     return false;
   }
+
   //Перенесем значения из activePiece на игровое поле
   //используем этод метод если достигли края поля или другой фигуры
   lockPiece() {
